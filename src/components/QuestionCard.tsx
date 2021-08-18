aa8f091fd9271586df96f3222eacf9ed14747b4d
uhyun-fe <feyouhyun0957@gmail.com>
2021-08-18 17:02:40 +0900

    create : styles.ts file & utills.ts
    - 예제 코드 마무리

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,31 +1,35 @@
 import React from "react";
 
+import { AnswerObject } from "../App";
+
+import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
+
 type Props = {
    question: string;
    answers: string[];
-   callback: any;
-   userAnswer: boolean;
+   callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+   userAnswer: AnswerObject | undefined;
    questionNr: number;
    totalQuestions: number;
 };
 
 const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNr, totalQuestions }) => {
    return (
-      <div>
+      <Wrapper>
          <p className="number">
-            Question: {question} / {totalQuestions}
+            Question: {questionNr} / {totalQuestions}
          </p>
          <p dangerouslySetInnerHTML={{ __html: question }} />
          <div>
             {answers.map((answer) => (
-               <div>
-                  <button disabled={userAnswer} onClick={callback}>
+               <ButtonWrapper key={answer} correct={userAnswer?.correctAnswer === answer} userClicked={userAnswer?.answer === answer}>
+                  <button disabled={!!userAnswer} value={answer} onClick={callback}>
                      <span dangerouslySetInnerHTML={{ __html: answer }}></span>
                   </button>
-               </div>
+               </ButtonWrapper>
             ))}
          </div>
-      </div>
+      </Wrapper>
    );
 };
 
